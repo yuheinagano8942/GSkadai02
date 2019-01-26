dc76264c1c45b9e95761fd4735631f948888a1c0
yuhei.nagano <yuheinagano8942@gmail.com>
2019-01-26 03:52:17 +0000

    first commit

---
+++ kadai.php
@@ -0,0 +1,12 @@
+
+<?php
+// Your code here!
+$profile = array("優平", "ハンバーグ", "読書");
+
+echo "私の名前は".$profile[0]."です";
+echo "<br>";
+echo "好きな食べ物は".$profile[1]."です";
+echo "<br>";
+echo "趣味は".$profile[2]."です";
+
+?>
